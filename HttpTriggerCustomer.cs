4c68a98dfbcdc872eea972331606959a3d065492
Doug bjj <costadelsolbjj@outlook.com>
2022-04-15 17:55:11 +0200

    changed method signatures

--- HttpTriggerCustomer.cs
+++ HttpTriggerCustomer.cs
@@ -12,11 +12,11 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Data.SqlClient;
 
-namespace OrderShippingFunc
+namespace CustomerFunc
 {
-    public static class OrderDetails
+    public static class CustomerDetails
     {
-        [FunctionName("OrderDetails")]
+        [FunctionName("CustomerDetails")]
         public static async Task<HttpResponseMessage> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
@@ -24,40 +24,40 @@ namespace OrderShippingFunc
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             // Get the customer's last name from the query string or the request body
-            string name = req.Query["name"];
+            string id = req.Query["id"];
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            id = id ?? data?.id;
 
-            Order requestedOrder = await getOrderAsync(name);
+            Customer requestedCustomer = await getCustomerAsync(id);
 
-            if (requestedOrder != null)
+            if (requestedCustomer != null)
             {
-                string orderJson = JsonConvert.SerializeObject(requestedOrder);
+                string customerJson = JsonConvert.SerializeObject(requestedCustomer);
                 return new HttpResponseMessage(HttpStatusCode.OK){
-                    Content = new StringContent(orderJson, Encoding.UTF8, "application/json")
+                    Content = new StringContent(customerJson, Encoding.UTF8, "application/json")
                 };
             }
             else
             {
                 return new HttpResponseMessage(HttpStatusCode.NotFound){
-                    Content = new StringContent("No order for that has been found for your request.")
+                    Content = new StringContent("No customer for that has been found for your request.")
                 };
             }
         }
 
-        private static async Task<Order> getOrderAsync(string customerLastName)
+        private static async Task<Customer> getCustomerAsync(string id)
         {
-            //This method simulates returning an order,
+            //This method simulates returning an customer,
             //e.g. from a database
             // Get the connection string from app settings and use it to create a connection.
     var str = Environment.GetEnvironmentVariable("sqldb_connection");
-     Order returnedOrder = new Order();
+     Customer returnedCustomer = new Customer();
            
            
              SqlConnection conn = new SqlConnection("Server=tcp:dougsqlserverproject.database.windows.net,1433;Initial Catalog=DougProjectSQLDB;Persist Security Info=False;User ID=dougproject;Password=Sqlserver1404!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
                 conn.Open();
-                var text = "SELECT [FirstName]  FROM [SalesLT].[Customer] where CustomerId=" + customerLastName  ;
+                var text = "SELECT [FirstName]  FROM [SalesLT].[Customer] where CustomerId=" + id  ;
 
                 SqlCommand cmd = new SqlCommand(text, conn);
              using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
@@ -65,7 +65,7 @@ namespace OrderShippingFunc
                     if (reader.Read())
                     {
                       
-                    returnedOrder.FirstName = String.Format("{0}",reader["FirstName"]);
+                    returnedCustomer.FirstName = String.Format("{0}",reader["FirstName"]);
                         
                     }
                     }
@@ -76,11 +76,11 @@ namespace OrderShippingFunc
 
          
 
-            return returnedOrder;
+            return returnedCustomer;
         }
     }
 
-    public class Order 
+    public class Customer 
     {
         public string FirstName { get; set; }
       
